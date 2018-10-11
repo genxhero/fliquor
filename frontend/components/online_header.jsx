a4b150cf21dc053e41834218ee108c2bfd5db176
Aaron Goddard <aaronbear@gmail.com>
2018-10-10 18:14:27 -0700

    Stylize search bar and links

--- frontend/components/online_header.jsx
+++ frontend/components/online_header.jsx
@@ -13,11 +13,15 @@ class Header extends React.Component {
 
            <div className="left-header">
              <Link to="/" className="home-link">fliquor</Link>
+             <div className="nav-menu">
+               <Link to="/home" className="nav-link">Explore</Link>
+               <h3 className="nav-link">You</h3>
+             </div>
            </div>
 
            <div className="right-header">
              <form className="online-search">
-               <input className="search-bar-main" type="text" placeholder="Photos" ></input>
+                 <input className="search-bar-main" type="text" placeholder="Photos" ></input>
              </form>
              <Link to="/" className="upload-link"></Link>
               {this.displayButtons()}
