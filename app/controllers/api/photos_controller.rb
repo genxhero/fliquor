029c888be599a2d34b1ef39f90d3fea5672d484b
Aaron Goddard <aaronbear@gmail.com>
2018-10-11 17:15:09 -0700

    Fix controller

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -6,6 +6,7 @@ class Api::PhotosController < ApplicationController
 
     def create
       @photo = Photo.new(photo_params)
+      @photo.user_id = current_user.id
       if @photo.save
         render :show
       else
